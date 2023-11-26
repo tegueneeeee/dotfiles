60dc9a5c8e5f79101738343255c92541b773c26a
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2023-11-26 17:40:33 +0900

    brwe

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,23 +1,29 @@
 #!/bin/sh
 alias j='z'
 alias f='zi'
-alias g='lazygit'
 
 # confirm before overwriting something
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
 
-alias m="git checkout master"
-alias s="git checkout stable"
-
+alias vi='nvim'
 alias vim='nvim'
+alias neovim='nvim'
+alias g='git'
+alias gi='git'
+alias c='clear'
 alias ls='exa'
 alias ll='ls -l'
 alias copypath='pwd | pbcopy' # copy current patht to clipboard
 alias o='open .'
-alias c='clear'
 alias cat='bat'
-
-alias java8='export PATH="$PATH":"/usr/libexec/java_home -v 1.8/bin"'
-alias java11='export PATH="$PATH":"/usr/libexec/java_home -v 11/bin"'
+alias .='cd ..'
+alias ..='cd ../..'
+alias ...='cd ../../..'
+alias ....='cd ../../../..'
+alias .....='cd ../../../../..'
+alias emu='emulator @$(emulator -list-avds | peco)'
+alias emu-ls='avdmanager list avd'
+alias simu='open -a Simulator'
+alias simu-ls='xcrun simctl list'
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,31 +1,19 @@
-#/bin/sh
-# HISTFILE="$XDG_DATA_HOME"/zsh/history
-HISTSIZE=1000000
-SAVEHIST=1000000
+export TERM='xterm-256color'
 export LANG=en_US.UTF-8
-export MANPAGER='nvim +Man!'
-export MANWIDTH=999
-export N_PREFIX=$HOME/.n
-export PATH=$PATH:$N_PREFIX/bin
-if [[ -n $SSH_CONNECTION ]]; then
-    export EDITOR='vim'
-else
-    export EDITOR='nvim'
-fi
+export EDITOR='nvim'
+export VISUAL='nvim'
+export PAGER='less'
 
-## Flutter
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$PATH":"$ANDROID_HOME/platform-tools"
+export PATH="$PATH":"$ANDROID_HOME/tools"
+export PATH="$PATH":"$ANDROID_HOME/tools/bin"
+export PATH="$PATH":"$ANDROID_HOME/emulator"
+export PATH="$PATH":"$ANDROID_HOME/cmdline-tools/latest/bin"
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 export PATH="$PATH":"$HOME/fvm/default/bin"
-## Fastlane
 export PATH="$PATH":"$HOME/.fastlane/bin"
-## Maestroi
 export PATH="$PATH":"$HOME/.maestro/bin"
-
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284"
-
 
 eval "$(zoxide init zsh)"
 eval "$(pyenv init --path)"
