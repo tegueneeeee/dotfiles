15660980255d3e21a7d27efd3cb3f3ab7b3ae435
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2023-12-05 18:57:53 +0900

    zsh exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -14,6 +14,9 @@ export PATH="$PATH":"$HOME/.pub-cache/bin"
 export PATH="$PATH":"$HOME/fvm/default/bin"
 export PATH="$PATH":"$HOME/.fastlane/bin"
 export PATH="$PATH":"$HOME/.maestro/bin"
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
 eval "$(zoxide init zsh)"
 eval "$(pyenv init --path)"
