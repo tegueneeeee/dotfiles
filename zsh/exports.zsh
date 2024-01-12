6567f49df23858483f0a280c62fdb02040f191d1
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2024-01-12 11:11:10 +0900

    skhd

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,6 +5,7 @@ export VISUAL='nvim'
 export PAGER='less'
 
 export ANDROID_HOME="$HOME/Library/Android/sdk"
+export ANDROID_SDK_ROOT="$HOME/Library//Android/sdk"
 export PATH="$PATH":"$ANDROID_HOME/platform-tools"
 export PATH="$PATH":"$ANDROID_HOME/tools"
 export PATH="$PATH":"$ANDROID_HOME/tools/bin"
