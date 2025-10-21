5945af73e19d1c03b1fb7e4b5d8c5af601be4652
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2025-10-21 18:24:57 +0900

    dotbot -> chezmoi

---
+++ dot_config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,15 @@
+abbr -a p pwd
+abbr -a c clear
+abbr -a vim nvim
+abbr -a reload exec fish
+abbr -a g git
+abbr -a lg lazygit
+abbr -a ls eza --icons --git --group-directories-first
+abbr -a ll eza --icons --git --group-directories-first --long
+abbr -a la eza --icons --git --group-directories-first --long -all
+abbr -a rm rm -i
+abbr -a mv mv -i
+abbr -a cp cp -i
+
+abbr -a cat bat
+abbr -a weather curl 'https://wttr.in'
---
+++ dot_config/fish/set-universal.fish
@@ -0,0 +1,10 @@
+# These variables will be written in ~/.config/fish/fish_variables
+# Run them once when you set up your system
+set -Ux EDITOR nvim
+set -Ux MANPAGER "nvim +Man! +'set nocursorcolumn scrolloff=999'"
+set -Ux LESSHISTFILE -
+
+set -q XDG_CACHE_HOME || set -Ux XDG_CACHE_HOME "$HOME/.cache"
+set -q XDG_CONFIG_HOME || set -Ux XDG_CONFIG_HOME "$HOME/.config"
+set -q XDG_DATA_HOME || set -Ux XDG_DATA_HOME "$HOME/.local/share"
+set -q XDG_STATE_HOME || set -Ux XDG_STATE_HOME "$HOME/.local/state"
