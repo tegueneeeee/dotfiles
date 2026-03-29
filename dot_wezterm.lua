e450169c688ee7261ee8cc52fd5c6d3f38cb4a75
Taekwon Kim (tegueneeeee) <kimxordnjs@naver.com>
2026-03-29 10:30:55 +0900

    up

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -11,7 +11,6 @@ end
 
 -- Settings
 config.default_prog = { fish_path, "-l" }
-
 config.color_scheme = "Catppuccin Frappe"
 local custom_colors = {
 	red = "#E78284",
@@ -37,133 +36,131 @@ config.inactive_pane_hsb = {
 }
 
 -- Keys
-config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	-- Send C-a when pressing C-a twice
-	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
-	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = ":", mods = "LEADER", action = act.ActivateCommandPalette },
-
-	-- Workspace (similar to session in Tmux)
-	{ key = "s", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
-
-	-- Tab (similar to window in Tmux)
-	{ key = "w", mods = "LEADER", action = act.ShowTabNavigator },
-	{ key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	-- Activate leader_mode using physical 'A' key (Works for both EN 'a' and KR 'ㅁ')
 	{
-		key = ",",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Renaming Tab Title...:" },
-			}),
-			action = wezterm.action_callback(function(window, _, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
+		key = "phys:A",
+		mods = "CTRL",
+		action = act.ActivateKeyTable({
+			name = "leader_mode",
+			one_shot = true,
 		}),
 	},
-	-- Key table for moving tabs around
-	{ key = ".", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
-	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-	--{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-	--{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
-
-	-- Pane
-	{ key = '"', mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "%", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "phys:Space", mods = "LEADER", action = act.RotatePanes("Clockwise") },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
-	{
-		key = "!",
-		mods = "LEADER | SHIFT",
-		action = wezterm.action_callback(function(_, pane)
-			local tab, window = pane:move_to_new_tab()
-		end),
-	},
-	-- We can make separate keybindings for resizing panes
-	-- But Wezterm offers custom "mode" in the name of "KeyTable"
-	{ key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
 }
 
--- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
+config.key_tables = {
+	-- Main command mode
+	leader_mode = {
+		-- Move to start of line (Ctrl+a)
+		{ key = "phys:A", mods = "CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+
+		-- Tab management
+		{ key = "phys:C", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "phys:W", action = act.ShowTabNavigator },
+		{ key = "phys:P", action = act.ActivateTabRelative(-1) },
+		{ key = "phys:N", action = act.ActivateTabRelative(1) },
+		{
+			key = "phys:Comma",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Renaming Tab Title...:" },
+				}),
+				action = wezterm.action_callback(function(window, _, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+		-- Switch to tab movement table
+		{ key = "phys:Period", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+
+		-- Pane management
+		{ key = '"', action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "%", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "phys:H", action = act.ActivatePaneDirection("Left") },
+		{ key = "phys:J", action = act.ActivatePaneDirection("Down") },
+		{ key = "phys:K", action = act.ActivatePaneDirection("Up") },
+		{ key = "phys:L", action = act.ActivatePaneDirection("Right") },
+		{ key = "phys:Space", action = act.RotatePanes("Clockwise") },
+		{ key = "phys:Z", action = act.TogglePaneZoomState },
+		{ key = "phys:X", action = act.CloseCurrentPane({ confirm = true }) },
+		{
+			key = "!",
+			mods = "SHIFT",
+			action = wezterm.action_callback(function(_, pane)
+				local tab, window = pane:move_to_new_tab()
+			end),
+		},
+
+		-- Utilities
+		{ key = "phys:LeftBracket", action = act.ActivateCopyMode },
+		{ key = ":", action = act.ActivateCommandPalette },
+		{ key = "phys:S", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+		{ key = "phys:R", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+
+	-- Key table for resizing panes
+	resize_pane = {
+		{ key = "phys:Comma", mods = "SHIFT", action = act.AdjustPaneSize({ "Left", 1 }) }, -- <
+		{ key = "phys:Minus", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "phys:Equal", mods = "SHIFT", action = act.AdjustPaneSize({ "Up", 1 }) }, -- +
+		{ key = "phys:Period", mods = "SHIFT", action = act.AdjustPaneSize({ "Right", 1 }) }, -- >
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+
+	-- Key table for moving tabs
+	move_tab = {
+		{ key = "phys:H", action = act.MoveTabRelative(-1) },
+		{ key = "phys:J", action = act.MoveTabRelative(-1) },
+		{ key = "phys:K", action = act.MoveTabRelative(1) },
+		{ key = "phys:L", action = act.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "Enter", action = "PopKeyTable" },
+	},
+}
+
+-- Quickly navigate tabs with index (inside leader_mode)
 for i = 1, 9 do
-	table.insert(config.keys, {
+	table.insert(config.key_tables.leader_mode, {
 		key = tostring(i),
-		mods = "LEADER",
 		action = act.ActivateTab(i - 1),
 	})
 end
 
-config.key_tables = {
-	resize_pane = {
-		{ key = "<", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "-", action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "+", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = ">", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-	},
-	move_tab = {
-		{ key = "h", action = act.MoveTabRelative(-1) },
-		{ key = "j", action = act.MoveTabRelative(-1) },
-		{ key = "k", action = act.MoveTabRelative(1) },
-		{ key = "l", action = act.MoveTabRelative(1) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-	},
-}
-
--- Tab bar
--- I don't like the look of "fancy" tab bar
+-- Tab bar appearance
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_bar_at_bottom = true
+
+-- Update status bar info
 wezterm.on("update-status", function(window, pane)
-	-- Workspace name
 	local stat = window:active_workspace()
 	local stat_color = custom_colors.red
-	-- It's a little silly to have workspace name all the time
-	-- Utilize this to display LDR or current key table name
+
 	if window:active_key_table() then
 		stat = window:active_key_table()
 		stat_color = custom_colors.teal
-	end
-	if window:leader_is_active() then
-		stat = "LDR"
-		stat_color = custom_colors.mauve
+		if stat == "leader_mode" then
+			stat = "LDR"
+			stat_color = custom_colors.mauve
+		end
 	end
 
 	local basename = function(s)
-		-- Nothing a little regex can't fix
 		return string.gsub(s, "(.*[/\\])(.*)", "%2")
 	end
 
-	-- Current working directory
 	local cwd = pane:get_current_working_dir()
-	if cwd then
-		cwd = basename(cwd.file_path) --> URL object introduced in 20240127-113634-bbcac864 (type(cwd) == "userdata")
-	-- cwd = basename(cwd) --> 20230712-072601-f4abf8fd or earlier version
-	else
-		cwd = ""
-	end
+	cwd = cwd and basename(cwd.file_path) or ""
 
-	-- Current command
 	local cmd = pane:get_foreground_process_name()
-	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
 	cmd = cmd and basename(cmd) or ""
 
-	-- Left status (left of the tab line)
 	window:set_left_status(wezterm.format({
 		{ Foreground = { Color = stat_color } },
 		{ Text = "  " },
@@ -171,10 +168,7 @@ wezterm.on("update-status", function(window, pane)
 		{ Text = " |" },
 	}))
 
-	-- Right status
 	window:set_right_status(wezterm.format({
-		-- Wezterm has a built-in nerd fonts
-		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
 		{ Text = " | " },
 		{ Foreground = { Color = custom_colors.yellow } },
@@ -182,16 +176,5 @@ wezterm.on("update-status", function(window, pane)
 		{ Text = "  " },
 	}))
 end)
-
---[[ Appearance setting for when I need to take pretty screenshots
-config.enable_tab_bar = false
-config.window_padding = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '0.5cell',
-  bottom = '0cell',
-
-}
---]]
 
 return config
